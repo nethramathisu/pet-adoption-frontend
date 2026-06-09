1fb352a81e72acefefdfb919655248b76fa4a908
nethramathisu <nethra.gmr@gmail.com>
2026-06-09 19:05:27 +0530

    baseurl fixed

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
-// baseURL:"https://pet-adoption-backend-oylk.onrender.com"
+//   baseURL: "http://localhost:5000",
+baseURL:"https://pet-adoption-backend-oylk.onrender.com"
 
 });
 
